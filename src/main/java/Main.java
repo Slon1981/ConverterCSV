aaa7f4887b0df52393316c58994d91cd1f536667
Yuriy Levakhin <slon1981-81@mail.ru>
2023-09-11 21:37:32 +0300

    v2.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,20 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-import java.io.FileNotFoundException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -26,13 +34,16 @@ public class Main {
 
 
         String json = listToJson(list);
+        writeString(json, "data.json");
 
-        writeString(json);
+        List<Employee> employees = parseXML("data.xml");
+        String json2 = listToJson(employees);
+        writeString(json2, "data2.json");
     }
 
-    private static void writeString(String json) {
+    private static void writeString(String json, String outputFile) {
         try {
-            FileWriter writer = new FileWriter("data.json");
+            FileWriter writer = new FileWriter(outputFile);
             writer.write(json);
             writer.flush();
         } catch (IOException e) {
@@ -45,15 +56,14 @@ public class Main {
         Gson gson = builder.create();
 
         Type listType = new TypeToken<List<Employee>>() {}.getType();
-
-
         return gson.toJson(list, listType);
     }
 
     public static List<Employee> parseCSV(String[] mapping, String fileName) {
 
+        FileReader fileReader = null;
         try {
-            FileReader fileReader = new FileReader(fileName);
+            fileReader = new FileReader(fileName);
             CSVReader csvReader = new CSVReader(fileReader);
 
             ColumnPositionMappingStrategy strategy = new ColumnPositionMappingStrategy();
@@ -64,14 +74,58 @@ public class Main {
             CsvToBeanBuilder csvToBeanBuilder = new CsvToBeanBuilder(csvReader);
             CsvToBean csvToBean = csvToBeanBuilder.withMappingStrategy(strategy).build();
 
-            fileReader.close();
 
            return csvToBean.parse();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            try {
+                fileReader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
 
+    }
+
+    private static List<Employee> parseXML(String file) {
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            Document document = builder.parse(file);
+
+            Node node = document.getDocumentElement();
+            NodeList list = node.getChildNodes();
+
+
+            List<Employee> employees = new ArrayList<>();
+
+            for (int i = 0; i < list.getLength(); i++) {
+                Node node2 = list.item(i);
+
+                if (node2.getNodeType() == Node.ELEMENT_NODE) {
+                    Element e = (Element)node2;
+
+                    String id = e.getElementsByTagName("id").item(0).getTextContent();
+                    String firstName = e.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = e.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = e.getElementsByTagName("country").item(0).getTextContent();
+                    String age = e.getElementsByTagName("age").item(0).getTextContent();
+
+                    Employee emp = new Employee(Long.valueOf(id), firstName,lastName,country, Integer.valueOf(age));
+                    employees.add(emp);
+                }
+
+            }
+
+            return employees;
+
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
